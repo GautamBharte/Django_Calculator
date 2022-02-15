8eedb63c75d987223974a8245eb48aea11ff6f0c
Gautam <gautambharte123@gmail.com>
2022-02-15 08:49:10 +0530

    Update app

--- vootingapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VootingappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vootingapp'
--- vootingapp/urls.py
+++
@@ -1,9 +0,0 @@
-from unicodedata import name
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('getquery',views.getquery,name='getquery'),
-    path('sortdata',views.sortdata,name="sortdata")
-]
